3e539fc0bcc91fc9fff11c244181dcf45a5e8616
Caue Gaspar Macrini <caue.macrini@iti.gov.br>
2026-02-02 17:14:19 -0300

    Fix: change outDir to dist to match Vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react( )],
   server: {
     proxy: {
       '/validate': 'http://127.0.0.1:5000'
     }
   },
   build: {
-    outDir: 'build'
+    outDir: 'dist' // Alterado de 'build' para 'dist'
   }
-})
+} )
